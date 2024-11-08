d999bbcba123fcfe887f8b540b27d34344e00261
Ramakrishna-23 <111562450+Ramakrishna-23@users.noreply.github.com>
2024-11-08 18:40:22 +0530

    Add files via upload

--- SortStack.java
+++ SortStack.java
@@ -5,6 +5,22 @@ import java.util.*;
 public class SortStack {
     public static void main(String[] args) {
         Stack<Integer> stack = new Stack<>();
+        stack.push(1);
+        stack.push(4);
+        stack.push(6);
+        stack.push(9);
+        stack.push(8);
+        stack.push(3);
+        stack.push(5);
+        stack.push(2);
+        stack.push(7);
+
+        stack = sort(stack);
+
+        while(!stack.isEmpty()){
+            int ele = stack.pop();
+            System.out.println(ele);
+        }
 
     }
 
@@ -18,9 +34,14 @@ public class SortStack {
         return stack;
     }
     public static Stack<Integer> insert(Stack<Integer> stack, int temp){
-        if( stack.isEmpty() || stack.get((stack.size()-1)) <= temp{
+        if( stack.isEmpty() || stack.get((stack.size()-1)) <= temp){
             stack.add(temp);
             return stack;
         }
+        int temp1 = stack.pop();
+        stack = insert(stack, temp);
+        stack.push(temp1);
+
+        return stack;
     }
 }
